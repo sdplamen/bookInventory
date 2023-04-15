a20ce6cc73e4a217b37444d5b2f71e0983556dc0
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-15 10:54:24 +0300

    Create nav.py

---
+++ models  /nav.py
@@ -0,0 +1,5 @@
+navbar = [
+    {'name': 'Home', 'function': 'index', 'title': 'Welcome to our Homepage!'},
+    {'name': 'Books', 'function': 'get_books', 'title': 'Book Inventory'},
+    {'name': 'PassGen', 'function': 'pass_gen', 'title': 'Password Generator'},
+    ]
